062883fb5ddceb93c9d0e426df6318624a0a0b24
Manu Cornet <m@ma.nu>
2022-02-14 17:31:33 +0000

    Add a couple of paths

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+app.use(express.static('static', {}));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! <a href="/new">Create new board</a>')
+});
+
+app.get('/new', (req, res) => {
+  res.send('Create a new board');
 });
 
 app.listen(port, () => {
