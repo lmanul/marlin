291fcfb5a673a85357458a64dc605d3975348156
Manu Cornet <m@ma.nu>
2022-03-02 17:48:28 +0000

    Add Cancel button to new question form

--- static/boardpage.js
+++ static/boardpage.js
@@ -1,10 +1,22 @@
+const setNewQuestionFormVisibility = (flag) => {
+  document.getElementById('board-add-question').style.display =
+      flag ? 'block' : 'none';
+  document.getElementById('board-add-question-button').style.display =
+      flag ? 'none' : 'inline-block';
+  if (flag) {
+    const area = document.getElementById('board-add-question-text');
+    area.focus();
+    area.addEventListener(
+      'input', onNewQuestionTextModified);
+  }
+};
+
 const showNewQuestionForm = () => {
-  document.getElementById('board-add-question').style.display = 'block';
-  document.getElementById('board-add-question-button').style.display = 'none';
-  const area = document.getElementById('board-add-question-text');
-  area.focus();
-  area.addEventListener(
-    'input', onNewQuestionTextModified);
+  return setNewQuestionFormVisibility(true);
+};
+
+const hideNewQuestionForm = () => {
+  return setNewQuestionFormVisibility(false);
 };
 
 const onNewQuestionTextModified = (e) => {
