fb0b882dbc7d897d4db596149b6de621c5e5082e
Manu Cornet <m@ma.nu>
2022-03-02 15:27:40 +0000

    Add 'Question' class

---
+++ model/question.js
@@ -0,0 +1,17 @@
+const util = require('../util');
+
+class Question {
+  constructor(id, boardId, text, context) {
+    this.id = id;
+    this.boardId = boardId;
+    this.text = text;
+    this.context = context;
+  }
+
+  static generateId() {
+    return util.oneRandomLetter() + util.oneRandomLetter() +
+      util.oneRandomLetter() + util.oneRandomLetter();
+  }
+}
+
+module.exports = Question;
