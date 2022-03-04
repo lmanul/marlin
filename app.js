c2a1bc8406405a27f3b2799333b41259d15fafe5
Manu Cornet <m@ma.nu>
2022-03-04 13:10:09 +0000

    Change default port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const app = express()
-const port = 8080
+const port = 80
 
 const authentication = require('./authentication');
 const createboard = require('./createboard');
