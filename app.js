de3d49d9c2d4dd540d4eaa9bcd3a5487b4e00538
Manu Cornet <m@ma.nu>
2024-04-17 06:25:08 +0000

    Oops, actually need 443 for https

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const app = express()
-const port = 80
+const port = 443
 
 const authentication = require('./authentication');
 const createboard = require('./createboard');
