ba23da41c7c409085aef82ab373b90a531167483
Manu Cornet <m@ma.nu>
2024-04-17 06:30:09 +0000

    Actually, probably need to use whatever port the platform tells us

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
-const port = 443
+const app = express();
+const port = process.env.PORT || 8888;
 
 const authentication = require('./authentication');
 const createboard = require('./createboard');
